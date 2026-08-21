99aa7d8415b8436772e2bccfac8636cb44e5b1b0
VenusIsJaded <VenusIsJaded@users.noreply.github.com>
2026-08-21 00:08:56 +0000

    Fix missing View import in MainActivity (was removed in cleanup)

--- app/src/main/java/com/dt/manager/MainActivity.java
+++ app/src/main/java/com/dt/manager/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Environment;
 import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.TextView;
